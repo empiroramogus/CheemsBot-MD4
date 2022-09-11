0a57a0193b5dfa98370d04f0241676ad762d715b
empiroramogus <110681795+empiroramogus@users.noreply.github.com>
2022-09-11 13:52:43 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['212621380740'] //ur owner number
+global.ownername = "Empiror Amogus" //ur owner name
+global.ytname = "YT: " //ur yt chanel name
+global.socialm = "GitHub: empiroramogus" //ur github or insta name
+global.location = "unknow" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['212621380740']
+global.ownertag = '212621380740' //ur tag number
+global.botname = 'Gustavo Fring V1' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCxuq8GjOkcNXxxNEA7GmJyw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCxuq8GjOkcNXxxNEA7GmJywhttps://www.youtube.com/channel/UCxuq8GjOkcNXxxNEA7GmJyw" //ur website to be displayed
+global.botscript = 'https://www.youtube.com/watch?v=dQw4w9WgXcQhttps://www.youtube.com/channel/UCxuq8GjOkcNXxxNEA7GmJywhttps://www.youtube.com/channel/UCxuq8GjOkcNXxxNEA7GmJyw' //script link
+global.reactmoji = "👍" //ur menu react emoji
+global.themeemoji = "💎" //ur theme emoji
+global.packname = "Los Pollos Hermonas" //ur sticker watermark packname
+global.author = "Gustavo Fring" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['212621380740'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,19 +102,19 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','-','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
+    success: 'تم ✓',
+    admin: 'الميزة متوفرة للاداريين فقط!',
+    botAdmin: 'يحتاج البوت الادمن الولا!',
+    owner: 'الميزة متوفرة للمطور فقط!',
+    group: 'الميزة متوفرة للقروب فقط!',
+    private: 'الميزة متوفرة في الخاص فقط!',
+    bot: 'الميزة متوفرة للبوت فقط',
+    wait: 'جاري المعالجة...',
+    linkm: 'اين الرابط?',
+    error: 'خطأ!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
